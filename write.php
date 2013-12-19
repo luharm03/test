c7abfdc17884c4470ac4ff1e3cbbb34b0038d04d
geetika <luharm03@gmail.com>
2013-12-19 15:50:44 +0530

    write.php

---
+++ write.php
@@ -0,0 +1,130 @@
+1) Pubdirecte banner appears in your advertising space. It disappears as soon as you have an advertiser. Note that any member entering Pubdirecte through our banner on your site will automatically become your affiliate and you will receive 10% of all his/her lifetime gains on the platform!
+
+
+
+
+2)The incentive is also called « forced click ». There are several kinds of incentives like paying visitors or simply offering them some kind of reward to get them to click on your ads.
+
+Any form of incentive is strictly prohibited on preconfigured ad placements (ad rotation), and is also prohibited for independent campaigns (external script), unless the incentive is indicated as available in the campaign description by the advertiser. We are actively monitoring editors to ensure they don't break this rule! 
+
+
+
+
+3)Question # 1 [Q]: I want to advertise my site but how does-it work?
+
+[A]: First, you need to fund your account. Several payment methods are available. You must then register your site as an advertiser in the section called « manage your sites ».
+
+Then, you can browse through the "Launch a new campaign" section in order to find sites you want your ads to be displayed on. You can choose to pay the publisher on a CPC or CPM basis. All the publishers have different rates, you can pick the ones that suit your needs best.
+
+
+
+
+4)1) DEFINITIONS
+
+Publishers: refers to persons or entities selling advertising space on their site using pubdirecte.com
+
+Advertisers: refers to persons or entities purchasing advertising through pubdirecte.com
+
+Payment methods:
+
+CPM = cost per thousand ad impressions.
+CLICK = action triggered when a user clicks on an advertisement.
+LEAD = action triggered when a user registers or validates a form on the advertiser's website.
+
+
+
+
+5)) PUBLISHER AND OBLIGATIONS
+
+2.1) Banner ad placement
+
+The advertising space must be placed on your site, a URL authorized by Pubdirecte.
+
+Maximum 5 banner ad placements per page, and there should be no more than 2 ads with the same dimensions.
+
+Exception: for external scripts (dedicated to a campaign) with a “pay per lead” or PPC remuneration, you can use an unlimited number of ad placements on your site.
+
+Pubdirecte's tags should be placed only in the french sections of your site.
+
+You can place the ads in another section of your site but we strongly advise that you mention it in the description of your site.
+It is strictly forbidden to place your ads in a cache, meaning that the advertiser must be able to view the advertising space on your site at any time!
+
+It is strictly forbidden to use banner ad placements on sites that you did not declare on Pubdirecte!
+
+The tag must be placed only on the pages hosted on the declared site.
+
+Using an iframe containing the Pubdirecte tag hosted on another website is strictly forbidden.
+
+
+
+
+6)To request a payment, you must have a minimum of $20 on your account. The payment must be requested between the 1st and 15th of each month, all payments being sent within the following 15 days!
+
+
+
+7)2.5) The publisher's duty
+
+For tax purposes, the publisher is required to report to the competent authorities all the revenues collected from Pubdirecte.com.
+
+Pubdirecte.com must not be held responsible for non-declaration of earnings from the publisher. It is our responsibility to pay the publishers for all the advertising gains they make on their sites with our banner ad placements. However, publishers solely are responsible for paying fees and taxes on their gains based on the laws from their country of residence.
+
+As a publisher, you agree to abide by this regulation. If you don't, your account will be deleted without notice or payment, a complaint may be filed with the relevant authorities following the litigation.
+
+
+
+
+
+
+8)2.6) Campaigns validation delay
+
+When an advertiser shows an interest for a campaign on one of your banner ad placements and you have enabled manual validation, you have up to 14 days to accept or reject the request. If you do not make your decision before 14 days, the application will be considered as accepted and validated automatically.
+
+
+
+
+9)2.2 Obligations for “pay-per-lead” campaigns
+2.4For “pay-per-lead” campaigns, the advertiser agrees not to remove the code (tag) that allows Pubdirecte to track the relevant data. If the tag is removed, the campaign will automatically switch to a  PPC type campaign, on the basis of $0.1 per click since the last recorded lead.
+
+
+
+
+10)2.23.4) Conditions of lead acceptance 
+2.3
+2.4Under $0.25  per lead, no special condition is available.
+2.5
+The leads must all be accepted, except in the cases of cheating, incomplete or just wrong registration.
+2.6
+If you have any restrictions on countries, use of proxies or any other similar restrictions, they must be placed prominently on your form.
+2.7
+If you want an advertising campaign with special conditions, they must be clearly specified when creating your campaign. For example: lead is validated if the member is active, and you must pay at least $0.25 if incentive is used, or $0.3 without incentive.
+
+
+
+
+
+
+11)2.2Publishers and advertisers acceptance guidelines
+2.3
+2.4In order to join Pubdirecte.com, sites should be in line with Swiss law, and not be indecent or immoral.
+2.5
+Your site must be French or have a French section.
+2.6
+The majority of your traffic (over 50%) of your site or french section must come from these countries:
+
+2.7- France
+- Switzerland
+- Belgium
+- Canada
+- Luxembourg
+
+2.8Adult sites are allowed, providing that a disclaimer in accordance to the law is used.
+
+2.9Sites must have real content.
+2.10
+You must own the proposed sites, or have permission from the owner to use them.
+2.11
+Pubdirecte.com reserves the right to refuse any site considered as not in line with its validation policy.
+2.12
+Acceptance of a Pubdirecte.com site does not indicate that it is in line with the law, meaning that you solely are responsible for the content you put on your site. Pubdirecte will not in any way be held responsible for the content displayed on your site.
+2.13
+If any legal problems occur, your contact details will be forwarded to the relevant jurisdiction.
